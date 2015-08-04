17c0904957e6fe8a4504bf469807afb040320092
Artem Zaborskii <compartia@gmail.com>
2015-08-04 19:10:09 +0300

    cv

---
+++ cars/gal/cv.js
@@ -0,0 +1,138 @@
+var artem_zaborskiy_cv = [
+
+		{
+			start : Date.parse("21 February, 2015"),
+			position : "freelance web developer",
+			employer : "this",
+			employerUrl : "http://www.zaborskiy.org/",
+
+			technologies : [ "Spring Core", "Spring security", "Hibernate", "ehCache", "AngularJS", "jQuery",
+					"Liquibase", "REST", "Dozer mapper", "JUnit", "DBUnit", "Mockito" ],
+			tools : [ "Eclipse", "Jira", "Jenkins", "Sonar", "git", "Confluence", "Maven" ],
+			teamSize : "1 - 3",
+			platforms : [ "Heroku", "Tomcat", "Android", "GitHub" ],
+			process : "Agile"
+		},
+
+		{
+			start : Date.parse("March, 2013"),
+			stop : Date.parse("21 February, 2015"),
+			position : "Lead Java Developer",
+
+			employer : "Return on Intelligence",
+			employerDescr : "former Exigen Services",
+			employerUrl : "http://www.returnonintelligence.com/",
+
+			process : "Scrum",
+			customer : "Carpathia",
+			project : "Carpathia hosting. Inventory management for a large hosting provider",
+			respons : [ "Requirements elicitation and analysis", "Technical design", "Business components development",
+					"DB design" ],
+			technologies : [ "Spring Core", "Spring security", "Hibernate", "ehCache", "AngularJS", "jQuery",
+					"Liquibase", "REST", "Dozer mapper", "JUnit", "DBUnit", "Mockito" ],
+			tools : [ "Eclipse", "Jira", "Jenkins", "Sonar", "SVN", "Confluence", "Maven" ],
+			teamSize : "14 - 30",
+			platforms : [ "Postgres", "Tomcat" ]
+		},
+
+		{
+
+			start : Date.parse("March, 2011"),
+			stop : Date.parse("March, 2013"),
+
+			position : "Lead Developer",
+			employer : "Exigen Services",
+
+			employerUrl : "http://www.returnonintelligence.com/",
+			customer : "Thompson Reuters, InPublic/MMI",
+			process : "Scrum",
+			project : "InPublic/MMI: A JEE web app for Thompson Reuters. The app provides functionality to publish and distribute press releases.",
+			respons : [ "Requirements collecting and analysis", "Technical design",
+					"Planning and tasks distribution, reporting, peer review, code review",
+					"Business components development" ],
+			technologies : [ "JEE", "Spring Framework", "Hibernate", "JSF", "GWT", "RichFaces", "Liquibase", "Lucene",
+					"JUnit" ],
+			tools : [ "Eclipse", "Jira", "TeamCity", "Jenkins", "Sonar", "Perforce", "Maven" ],
+			achievments : "Has developed the architecture and the framework for the MMI system. Has build the GWT based GUI for the MMI.",
+			platforms : [ "WebLogic", "OC4J", "Oracle" ],
+			teamSize : "7 - 10",
+
+		},
+
+		{
+			start : Date.parse("August, 2010 "),
+			stop : Date.parse("March, 2011"),
+
+			position : "Project Manager",
+			employer : "Exigen Services",
+			employerDescr : "acquired by Return on Intelligence",
+			employerUrl : "http://www.returnonintelligence.com/",
+			customer : "T-Mobile, DE",
+			project : "eCare --//-- see below",
+
+			respons : [ "Requirements analysis", "Planning and tasks distribution, time tracking, reporting",
+					"Interaction with Germany based business analysts",
+					"Staffing, team skills development, career development of employees. Coaching and organizing educational activities" ],
+			process : "Waterfall",
+			tools : [ "MS Project", "Jira" ],
+			teamSize : "15 - 25"
+		},
+
+		{
+			start : Date.parse("July, 2008"),
+			stop : Date.parse("August, 2010"),
+
+			position : "Lead Java Developer",
+			employer : "Exigen Services",
+			employerDescr : "acquired by Return on Intelligence",
+			employerUrl : "http://www.returnonintelligence.com/",
+			customer : "T-Mobile, DE",
+			project : "eCare. A distributed JEE app for T-Mobile (DE). The app provides functionality to manipulate customer’s contract data and tariff plans. The services are accessible from web and via web services (B2B SOA interface). One of the challenging things was integration with the number of partner systems.",
+
+			respons : [ "Requirements analysis", "Technical designs",
+					"Planning and tasks distribution, tracking, reporting", "Business components development",
+					"Interaction with business analysts located in Germany",
+					"Staffing, team members skills development" ],
+			tools : [ "Eclipse", "ClearCase", "Clear DDTS,", "Jira", "Checkstyle", "FindBugs", "Sonar", "Hudson" ],
+			technologies : [ "JEE", "Spring Framework", "Hibernate", "CXF", "XFire" ],
+			platforms : [ "WebLogic", "Oracle" ],
+			awards : "2008: Exigen, Best Technical Lead of the year",
+			process : "Waterfall",
+			teamSize : "15 - 25"
+		},
+
+		{
+			start : Date.parse("June, 2008"),
+			stop : Date.parse("September, 2008"),
+
+			position : "Teacher",
+			employer : "Exigen Services",
+			employerDescr : "acquired by Return on Intelligence",
+			employerUrl : "http://www.returnonintelligence.com/",
+
+			customer : "IT-college",
+
+			respons : [ "Teaching the group of 20 students" ],
+			technologies : [ "Java SE" ]
+		},
+
+		{
+			start : Date.parse("June, 2008"),
+			stop : Date.parse("September, 2008"),
+
+			position : "Senior Java Developer",
+			employer : "StarSoft",
+			employerDescr : "later acquired by Exigen Services",
+			employerUrl : "http://www.returnonintelligence.com/",
+
+			customer : "T-Mobile DE",
+
+			respons : [ "Business components development. Scope: all the tiers of the app: from GUI to DB",
+					"Architectural improvements" ],
+			technologies : [ "JEE", "Spring Framework", "Hibernate", "Axis", "XFire" ],
+			platforms : [ "WebLogic", "Oracle" ],
+			teamSize : "7 - 20",
+			process : "Waterfall"
+		}
+
+];
