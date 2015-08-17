4bbdc63b51bfcd2fbe8b9cc4872f4008828a783c
Artem Zaborskii <compartia@gmail.com>
2015-08-17 16:45:31 +0300

    css

---
+++ cars/assets/make.sh
@@ -0,0 +1,6 @@
+lessc -verbose -x --strict-imports less/tutorial.less css/tutorial.css
+lessc -verbose -x --strict-imports less/zaborskiy.less css/zaborskiy.css
+lessc -verbose -x --strict-imports less/cv.less css/cv.css
+
+lessc -verbose -x --strict-imports less/artem-patch.less css/cars.css
+
