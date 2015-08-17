6e8caf803dcf61c800a66a1bde69451f49f7dda9
Artem Zaborskii <compartia@gmail.com>
2015-08-17 22:17:34 +0300

    fixing links
    GitHub repos that use submodules will generally not deploy correctly on
    Heroku. This is because GitHub does not include submodule contents when
    repo-content tarballs are generated.
    
    so, make.sh just copies all

--- cars/make.sh
+++ cars/make.sh
@@ -1,6 +1,14 @@
+cd assets
 lessc -verbose -x --strict-imports less/tutorial.less css/tutorial.css
 lessc -verbose -x --strict-imports less/zaborskiy.less css/zaborskiy.css
 lessc -verbose -x --strict-imports less/cv.less css/cv.css
 
 lessc -verbose -x --strict-imports less/artem-patch.less css/cars.css
+
+
+cd ..
+cd neural
+git fetch
+cd ..
+cp neural/* gal/
 
