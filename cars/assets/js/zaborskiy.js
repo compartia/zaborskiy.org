24c4ec27b209fdbe56fbe71be4926fbaead3d600
Artem Zaborskii <compartia@gmail.com>
2015-11-16 03:43:21 +0300

    migration to scss, restyle

--- cars/assets/js/zaborskiy.js
+++ cars/assets/js/zaborskiy.js
@@ -21,3 +21,39 @@ zaborskiy
 						template : '<div class="img-div"><img class="cld-responsive" style="width: 100%" data-src="http://res.cloudinary.com/zaborskiy/image/upload/w_auto,dpr_auto/w_60,g_south_east,x_5,y_5,l_watermark/v1441311652/{{img}}"/><div ng-show="{{title}}" class="img-title">{{title}}</div></div>'
 					}
 				});
+
+zaborskiy.directive(
+
+'gridBox', function() {
+	return {
+		transclude : true,
+		scope : {
+			link : '@',
+			title : '@'
+		},
+		restrict : 'AC',
+		template : '<h3>{{title}}</h3><small><ng-transclude></ng-transclude></small>'
+	}
+});
+
+zaborskiy.directive(
+
+'gridPanel', function() {
+	return {
+		transclude : true,
+		scope : {
+			bg : '@',
+			bgClass : '@',
+			title : '@',
+			description : '@'
+		},
+		restrict : 'C',
+		template :
+		// '<a target="{{il.target}}" href="{{il.link}}">'+
+		'<div class="gallery-item gallery-item-tall" style="background-image: url(\'{{bg}}\')">'
+				+ '<div class="desciption-wrap {{bgClass}}">' + '<h4>{{title}}</h4>'
+				+ '<small ng-show="description">{{description}}</small>' + '<small><ng-transclude></ng-transclude> '
+				+ '</small></div>' + '</div>'
+	// '</a>'
+	}
+});
