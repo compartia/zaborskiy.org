e07c2029f6f68a52f86ff57475151b70e2c661b1
Artem Zaborskii <compartia@gmail.com>
2016-12-03 23:04:08 +0800

    add article about japan. 1st draft

--- web/Gruntfile.js
+++ web/Gruntfile.js
@@ -1,23 +1,46 @@
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
-      }
-    }
-  });
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
 
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
 
-  // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+            build: {
+                src: 'src/<%= pkg.name %>.js',
+                dest: 'build/<%= pkg.name %>.min.js'
+            }
+        },
+
+        sass: {
+            dist: {
+                files: {
+                    'assets/css/zaborskiy.css': 'assets/scss/zaborskiy.scss',
+                    'assets/css/tutorial.css': 'assets/scss/tutorial.scss'
+                }
+            }
+        },
+
+        watch: {
+
+            css: {
+                files: 'assets/scss/*.scss',
+                tasks: ['sass']
+            }
+
+
+        }
+    });
+
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+
+    // Default task(s).
+    grunt.registerTask('default', ['uglify', 'sass']);
 
 };
