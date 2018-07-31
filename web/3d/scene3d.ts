21e5e57fdb67a34250fef232cf33ba61ebaa24e8
Artem Zaborskiy <compartia@gmail.com>
2018-08-01 02:41:49 +0300

    Webpack (#6)
    * 3d
    
    * Web pack

---
+++ web/3d/scene3d.ts
@@ -0,0 +1,123 @@
+import * as THREE from 'three-full'
+
+
+export abstract class SimpleScene {
+    scene: THREE.Scene;
+    renderer: THREE.WebGLRenderer;
+    camera: THREE.PerspectiveCamera;
+
+    container: HTMLElement;
+    private light1: THREE.Light;
+    pointLight: THREE.Light;
+
+    controls: THREE.OrbitControls;
+    postprocessing: any;
+
+    constructor(container: HTMLElement) {
+        this.container = container;
+        this.scene = new THREE.Scene()
+
+        let fogColor = new THREE.Color(0x000011);
+
+        this.scene.background = fogColor;
+        this.scene.fog = new THREE.Fog(fogColor.getHex(), 0.05, 9);
+
+        let renderer = new THREE.WebGLRenderer()
+        renderer.setPixelRatio(window.devicePixelRatio);
+        renderer.gammaInput = true;
+        renderer.gammaOutput = true;
+        this.renderer = renderer;
+
+        // set size
+        this.renderer.setSize(container.clientWidth, container.clientHeight);
+
+        // add canvas to dom
+        container.appendChild(this.renderer.domElement);
+
+
+        this.resetScene();
+
+        window.addEventListener( 'resize', ()=>this.onWindowResize(), false );
+
+    }
+
+    private onWindowResize() {
+        
+        this.renderer.setSize( window.innerWidth, window.innerHeight );
+    }
+
+    public resetScene() {
+
+        while (this.scene.children.length > 0) {
+            this.scene.remove(this.scene.children[0]);
+        }
+
+        this.makeLights();
+        this.makeCamera();
+        this.makeObjects();
+
+    }
+
+
+    private initControls() {
+        let controls = new THREE.OrbitControls(this.camera, this.renderer.domElement);
+
+        controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
+        controls.dampingFactor = 0.25;
+        // controls.screenSpacePanning = false;
+        controls.minDistance = 0.5;
+        controls.maxDistance = 7
+
+        controls.enableZoom = true;
+        controls.enablePan = true;
+
+        controls.autoRotate = true;
+        controls.autoRotateSpeed = 0.3;
+
+        controls.maxPolarAngle=Math.PI*0.66;
+        controls.minPolarAngle=Math.PI*0.33;
+
+        this.controls = controls;
+
+    }
+
+
+
+    public abstract makeObjects();
+
+
+    private makeLights() {
+        // add lights
+        {
+            let light0 = new THREE.PointLight(0xffffff, 1.6)
+            light0.position.set(0, 0, 0)
+            this.scene.add(light0);
+            this.pointLight = light0;
+        }
+
+        this.light1 = new THREE.DirectionalLight(0x0f1de2, 0.9)
+        this.light1.position.set(100, 100, 100)
+        this.scene.add(this.light1);
+
+    }
+
+    private makeCamera() {
+        // create the camera
+        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+        this.camera.position.x = 2
+        this.camera.position.y = 1.5
+        this.camera.position.z = 2
+
+        this.camera.lookAt(this.scene.position);
+
+        this.initControls();
+        this.controls.update();
+    }
+
+
+    public abstract render(): void;
+
+
+
+}
---
+++ webpack.config.js
@@ -0,0 +1,141 @@
+/* webpack.config.js */
+
+const devMode = process.env.NODE_ENV !== 'production'
+
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const Clean = require('clean-webpack-plugin');
+const path = require('path');
+const setupServerMockup = require('./demo/serverMockup');
+
+
+/* Configure BrowserSync */
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
+const BrowserSyncPluginConfig = new BrowserSyncPlugin({
+    host: 'localhost',
+    port: 9020,
+    proxy: 'http://localhost:8080/'
+}, config = {
+    reload: false
+})
+
+
+module.exports = {
+    // Tell Webpack which file kicks off our app.
+    entry: {
+        // index: path.resolve(__dirname, 'web/index.html'),
+        index: path.resolve(__dirname, 'web/index.ts'),
+        japan: path.resolve(__dirname, 'web/gal/japan.ts'),
+        cv: path.resolve(__dirname, 'web/gal/cv.ts'),
+    },
+
+    resolve: {
+        modules: [
+            path.resolve(__dirname, 'bower_components'),
+            path.resolve(__dirname, 'node_modules'),
+        ],
+        extensions: ['.ts', '.tsx', '.js', '.jsx', '.html']
+    },
+    // These rules tell Webpack how to process different module types.cd ..
+    // Remember, *everything* is a module in Webpack. That includes
+    // CSS, and (thanks to our loader) HTML.
+
+    mode: "development",
+    module: {
+        rules: [
+            // { // css / sass / scss loader for webpack
+            //     test: /\.(css|sass|scss)$/,
+            //     use: ExtractTextPlugin.extract({
+            //       use: ['css-loader', 'sass-loader'],
+            //     })
+            // },
+
+            {
+                test: /\.(png|jpg|woff|woff2|eot|ttf|svg|pdf)$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 8000, // Convert images < 8kb to base64 strings
+                    name: 'assets/img/[name].[ext]'
+                }
+            },
+
+
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+
+            // {
+            //     test: /\.html$/,
+            //     loader: 'html-loader'
+            // },
+
+
+            {
+                // If you see a file that ends in .js, just send it to the babel-loader.
+                test: /\.js$/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+            },
+        ]
+    },
+    plugins: [
+        // This plugin will generate an index.html file for us that can be used
+        // by the Webpack dev server. We can give it a template file (written in EJS)
+        // and it will handle injecting our bundle for us.
+
+        new HtmlWebpackPlugin({
+            inject: false,
+            template: path.resolve(__dirname, 'web/index.html')
+        }),
+
+        new HtmlWebpackPlugin({
+            minify: true,
+            inject: true,
+            filename: 'japan.html',
+            template: 'web/gal/japan.html'
+        }),
+
+
+        new HtmlWebpackPlugin({
+            filename: 'artem_zaborskiy_resume.html',
+            template: 'web/gal/artem_zaborskiy_resume.html'
+        }),
+
+        // This plugin will copy files over to ‘./dist’ without transforming them.
+        // That's important because the custom-elements-es5-adapter.js MUST
+        // remain in ES2015. We’ll talk about this a bit later :)
+        new CopyWebpackPlugin([{
+            from: path.resolve(__dirname, 'bower_components/webcomponentsjs/*.js'),
+            to: 'bower_components/webcomponentsjs/[name].[ext]'
+        }]),
+
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: devMode ? '[name].css' : '[name].[hash].css',
+            chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+        }),
+
+        BrowserSyncPluginConfig,
+
+        new Clean(['dist']),
+    ],
+
+    // devServer: {
+    //     contentBase: path.join(__dirname),
+    //     compress: true,
+    //     overlay: true,
+    //     port: 9020,
+    //     setup: setupServerMockup,
+    // },
+};
