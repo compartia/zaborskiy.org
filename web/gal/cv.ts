0af34b1bba9b553c8fad24d24e45949d51c658a6
Artem Zaborskiy <compartia@gmail.com>
2018-08-01 14:13:07 +0300

    Heroic-travis conf

--- web/gal/cv.ts
+++ web/gal/cv.ts
@@ -1,4 +1,9 @@
 import '../assets/img/avatar.jpg'
+
+import '../assets/img/grid.svg'
+import '../assets/img/arrow.svg'
+import '../assets/img/marker-h.svg'
+
 import '../assets/img/az_side_branding1.svg'
 import '../assets/img/az_side_branding2.svg'
 import '../assets/img/az_side_branding3.svg'
