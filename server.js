32da1b9698a89fda0937424066f7db076911f0e8
Artem Zaborskiy <compartia@gmail.com>
2018-08-01 13:42:23 +0300

    Configure heroku

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'dist'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('dist/index.html'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+"use strict";
+exports.__esModule = true;
+var express = require("express");
+var path = require("path");
+var PORT = process.env.PORT || 5000;
+express().use(express.static(path.join(__dirname, 'dist')))
+    .listen(PORT, function () { return console.log("Listening on " + PORT); });
